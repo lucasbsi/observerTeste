034397eba0c58184334bd9aa35db89bfa9c29c06
Lucas Santana <lucas.bsi@outlook.com>
2020-11-26 19:53:34 -0300

    Interface Swing

---
+++ src/swing_package/tela.java
@@ -0,0 +1,16 @@
+package swing_package;
+
+import javax.swing.JFrame;
+
+public class tela {
+
+	public static void main(String[] args) {
+		
+		JFrame janela = new JFrame("Minha tela");
+		janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		janela.setVisible(true);
+
+	}
+
+}
